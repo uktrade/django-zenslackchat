2470ab6f07bfa83a952e6ba5d95be52c7a180ee6
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2025-02-19 12:05:40 +0000

    set env

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -5,4 +5,4 @@ set -e
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
 echo "Running collectstatic"
-python manage.py collectstatic --noinput
+DJANGO_SETTINGS_MODULE=config.settings.build python manage.py collectstatic --noinput
